1af4fb95db64287e7e89e8bff954e407f231f508
Fiona DELSING <fdelsing@e3r6p6.42.fr>
2018-01-03 19:13:57 +0100

    gnl fini a la norme

--- get_next_line.c
+++ get_next_line.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   gnl3.c                                             :+:      :+:    :+:   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: fdelsing <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2017/12/15 21:27:20 by fdelsing          #+#    #+#             */
-/*   Updated: 2017/12/15 23:56:07 by fdelsing         ###   ########.fr       */
+/*   Created: 2018/01/03 18:24:29 by fdelsing          #+#    #+#             */
+/*   Updated: 2018/01/03 18:24:48 by fdelsing         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,98 +23,49 @@ size_t		ft_len(char *sample)
 	return (i);
 }
 
-int		get_next_line(const int fd, char **line)
+int			ft_ret(char **temp, char **sample, char **line, int ret)
+{
+	size_t	len;
+
+	len = ft_len(*sample);
+	*line = ft_strsub(*sample, 0, len);
+	if (ret == 0 && ft_strlen(*sample) == 0)
+	{
+		free(*sample);
+		return (0);
+	}
+	*temp = ft_strsub(*sample, len + 1, ft_strlen(*sample) - len);
+	free(*sample);
+	*sample = ft_strsub(*temp, 0, ft_strlen(*temp));
+	free(*temp);
+	return (1);
+}
+
+int			get_next_line(const int fd, char **line)
 {
 	int			ret;
-	size_t		len;
 	char		*buf;
+	char		*temp;
 	static char *sample = "";
 
-
-	if (fd < 0)
-		return (-1);
-	buf = ft_strnew(BUFF_SIZE); 
-	while(ft_strchr(sample, '\n') == NULL)
+	while (ft_strchr(sample, '\n') == NULL)
 	{
+		buf = ft_strnew(BUFF_SIZE);
 		ret = read(fd, buf, BUFF_SIZE);
-		buf[ret] = '\0';
 		if (ret == -1)
 		{
 			free(buf);
 			return (-1);
 		}
-		sample = ft_strjoin(sample, buf);
-		if (ret < BUFF_SIZE)
-		{
-			len = ft_len(sample);
-			*line = ft_strsub(sample, 0, len);
-//			ft_putstr("sample = ");
-//			ft_putendl(sample);
-			free(buf);
+		buf[ret] = '\0';
+		temp = ft_strjoin(sample, buf);
+		free(buf);
+		if (ft_strcmp(sample, "") != 0)
 			free(sample);
-			if (ret == 0 && ft_strlen(sample) == 0)
-				return (0);
-			sample = ft_strsub(sample, len + 1, ft_strlen(sample) - len);
-			return (1);
-		}
+		sample = ft_strsub(temp, 0, ft_strlen(temp));
+		free(temp);
+		if (ret < BUFF_SIZE)
+			return (ft_ret(&temp, &sample, line, ret));
 	}
-//	ft_putstr("sample = ");
-//	ft_putendl(sample);
-	len = ft_len(sample);
-	*line = ft_strsub(sample, 0, len);
-	free(sample);
-	sample = ft_strsub(sample, len + 1, ft_strlen(sample) - len);
-	free(buf);
-//	ft_putstr("sample = ");
-//	ft_putendl(sample);
-	return (1);
-}
-
-int		main2(int argc, char **argv)
-{
-	int fd;
-	int i;
-	char *line;
-
-	fd = open(argv[1], O_RDONLY);
-	i = 0;
-
-	ft_putnbr(get_next_line((const int)fd, &line));
-	ft_putchar('\n');
-	ft_putendl(line);
-	ft_putnbr(get_next_line((const int)fd, &line));
-	ft_putchar('\n');
-	ft_putendl(line);
-	ft_putnbr(get_next_line((const int)fd, &line));
-	ft_putchar('\n');
-	ft_putendl(line);
-	ft_putnbr(get_next_line((const int)fd, &line));
-	ft_putchar('\n');
-	ft_putendl(line);
-	ft_putnbr(get_next_line((const int)fd, &line));
-	ft_putchar('\n');
-	ft_putendl(line);
-	ft_putnbr(get_next_line((const int)fd, &line));
-	ft_putchar('\n');
-	ft_putendl(line);
-	ft_putnbr(get_next_line((const int)fd, &line));
-	ft_putchar('\n');
-	ft_putendl(line);
-	//	while (get_next_line((const int)fd, &line) != 0)
-	//	{
-	//		ft_putnbr(get_next_line((const int)fd, &line));
-	//		ft_putchar('\n');
-	//
-	//		ft_putstr("line = ");
-	//		ft_putendl(line);
-	//	}
-	return (0);
-}
-
-int		main(int argc, char **argv)
-{
-	main2(argc, argv);
-		while (1)
-		{}
-	return (0);
+	return (ft_ret(&temp, &sample, line, ret));
 }
--- get_next_line.h
+++ get_next_line.h
@@ -6,18 +6,18 @@
 /*   By: fdelsing <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/12/14 19:40:58 by fdelsing          #+#    #+#             */
-/*   Updated: 2017/12/15 23:23:04 by fdelsing         ###   ########.fr       */
+/*   Updated: 2018/01/03 18:23:57 by fdelsing         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
-#include <stdlib.h>
-#include "Libft/libft.h"
-#include <fcntl.h>
+# include <stdlib.h>
+# include "Libft/libft.h"
+# include <fcntl.h>
 
-#define BUFF_SIZE 15
+# define BUFF_SIZE 9999
 
 int get_next_line(const int fd, char **line);
 
